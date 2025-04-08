dc12fa5e625535395dff5c7f4340e1882fafbca5
Jorh69 <Jorh@yandex.ru>
2025-04-08 05:13:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ def index():
 def webhook():
     if request.headers.get('content-type') == 'application/json':
         json_string = request.get_data().decode('utf-8')
+        logger.info(f"Parsed update: {update}")
         update = types.Update.de_json(json_string)
         bot.process_new_updates([update])
         return '', 200
